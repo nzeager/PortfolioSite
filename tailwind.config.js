8a81f9e18d0936d0c4c475340ba10be7035730f4
Nathan Zeager <nathanzeager@hotmail.com>
2024-02-01 12:24:34 -0500

    Implemented tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,tsx}",
+    "./**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
